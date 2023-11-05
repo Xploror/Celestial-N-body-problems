7f03ec35cae3b85411a3f0382028fb5305b4ea71
Xploror <suryansharyan01@gmail.com>
2023-11-05 12:02:47 -0500

    included jovian and analysis files

---
+++ Jovian_system.m
@@ -0,0 +1,114 @@
+clc;clear
+
+dt = 0.01;
+transfer = 0;
+
+G = 2*10^(-6);
+M = 19000000; % Jupiter  1.9*10^27
+m_s1 = 893;  % Io  8.93*10^22
+m_s2 = 480;  % Europa   4.8*10^22
+m_s3 = 1480;  % Ganymede  14.8*10^22
+m_sat = 0.0001;
+
+resonance_ratio = [2,1];
+e_s1 = 0.0041;  % Io
+e_s2 = 0.009;   % Europa
+e_s3 = 0.0013;  % Ganymede
+e_sat = e_s2;
+a_s1 = 1;  % Io 421700
+a_s2 = 670900/421700;   % Europa  670900
+a_s3 = 1070400/421700;   % Ganymede 1070400
+T_s1 = ((4*pi^2/(G*M))*a_s1^3)^(1/2);
+T_s2 = ((4*pi^2/(G*M))*a_s2^3)^(1/2);
+T_s3 = ((4*pi^2/(G*M))*a_s3^3)^(1/2);
+
+T_sat = resonance_ratio(2)*T_s2/resonance_ratio(1);
+a_sat_target = a_s2*(T_sat/T_s2)^(2/3);
+
+r_s1_init = [a_s1*(1+e_s1),0];
+r_s2_init = [a_s2*(1+e_s2),0];
+r_s3_init = [a_s3*(1+e_s3),0];
+v1_init = [0,sqrt(G*M*((2/r_s1_init(1))-(1/a_s1)))];
+v2_init = [0,sqrt(G*M*((2/r_s2_init(1))-(1/a_s2)))];
+v3_init = [0,sqrt(G*M*((2/r_s3_init(1))-(1/a_s3)))];
+
+%%%%% Transfer mechanism (Starts with an arbitrary parking orbit around primary 1)
+e1_sat = 0; % Circular orbit (Hyperparameter)
+a1_sat = a_sat_target*(1-e_sat); % (Hyperparameter)
+T1_sat = ((4*pi^2/(G*M))*a1_sat^3)^(1/2);
+r_sat_init = [a1_sat*(1+e1_sat),0];
+v_sat_init = sqrt(G*M*((2/norm(r_sat_init))-(1/a1_sat)))*cross([0,0,1], [r_sat_init,0])*[1 0 0; 0 1 0]'/norm(r_sat_init);
+t_trans = 40*T1_sat/2;  % (Hyperparameter)
+
+%Dynamic propogation
+r_s1 = r_s1_init;
+r_s2 = r_s2_init;
+r_s3 = r_s3_init;
+v_s1 = v1_init;
+v_s2 = v2_init;
+v_s3 = v3_init;
+r_sat = r_sat_init;
+v_sat = v_sat_init;
+t = 0;
+
+for i=0:dt:100
+    acc_s1 = -((G*M/norm(r_s1)^3)*r_s1 + (G*m_sat/norm(r_sat-r_s1)^3)*(r_s1 - r_sat) + (G*m_s2/norm(r_s2-r_s1)^3)*(r_s1 - r_s2) + (G*m_s3/norm(r_s3-r_s1)^3)*(r_s1 - r_s3));
+    acc_s2 = -((G*M/norm(r_s2)^3)*r_s2 + (G*m_sat/norm(r_sat-r_s2)^3)*(r_s2 - r_sat) + (G*m_s1/norm(r_s2-r_s1)^3)*(r_s2 - r_s1) + (G*m_s3/norm(r_s3-r_s2)^3)*(r_s2 - r_s3));
+    acc_s3 = -((G*M/norm(r_s3)^3)*r_s3 + (G*m_sat/norm(r_sat-r_s3)^3)*(r_s3 - r_sat) + (G*m_s2/norm(r_s2-r_s3)^3)*(r_s3 - r_s2) + (G*m_s1/norm(r_s3-r_s1)^3)*(r_s3 - r_s1));
+    acc_sat = -((G*M/norm(r_sat)^3)*r_sat + (G*m_s1/norm(r_sat-r_s1)^3)*(r_sat - r_s1) + (G*m_s2/norm(r_sat-r_s2)^3)*(r_sat - r_s2) + (G*m_s3/norm(r_sat-r_s3)^3)*(r_sat - r_s3));
+    v_s1 = v_s1 + acc_s1*dt;
+    v_s2 = v_s2 + acc_s2*dt;
+    v_s3 = v_s3 + acc_s3*dt;
+    v_sat = v_sat + acc_sat*dt;
+    r_s1 = r_s1 + v_s1*dt;
+    r_s2 = r_s2 + v_s2*dt;
+    r_s3 = r_s3 + v_s3*dt;
+    r_sat = r_sat + v_sat*dt;
+    
+    %%%%%%%%%%%%% Transfer mechanism %%%%%%%%%%%%%%%
+%     if t >= t_trans && transfer == 0
+%         a_sat = (norm(r_sat) + a_sat_target*(1+e_sat))/2;
+%         v_sat = sqrt(G*M*((2/norm(r_sat))-(1/a_sat_target)))*cross([0,0,1], [r_sat,0])*[1 0 0; 0 1 0]'/norm(r_sat_init);  % This line of code is analogous to delta_v thrust point
+%         transfer = 1;
+%     end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    figure(1)
+    plot(r_s1(1), r_s1(2), '.r', r_s2(1), r_s2(2), '.b', r_s3(1), r_s3(2), '.g', 0,0, '.k', r_sat(1), r_sat(2), '.y', 'MarkerSize', 20);
+    xlim([-3,3])
+    ylim([-3,3])
+    
+    %%%%%% This is the plot to visualize the celestial crossing of the
+    %%%%%% satellite 
+%     if abs((r_s/norm(r_s))*(r_sat/norm(r_sat))' - 1) <= 0.0005
+%         figure(2)
+%         plot(r_s(1), r_s(2), '.b', r_sat(1), r_sat(2), '.g')
+%         xlim([-5,5])
+%         ylim([-5,5])
+%         hold on;
+%     end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Contour plot
+    x = -3:0.05:3;
+    y = -3:0.05:3;
+    [X, Y] = meshgrid(x,y);
+    len_x = length(X);
+    len_y = length(Y);
+    U = -(G*M*10^(-4)./sqrt(X.^2 + Y.^2) + G*m_s1./sqrt((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2) + G*m_s2./sqrt((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2) + G*m_s3./sqrt((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2));
+    figure(2)
+    contourf(X, Y, U);
+    hold on;
+ 
+    R = sqrt(X.^2+Y.^2);
+    acc_obj_x = -((G*10^(-3)*M./R^3).*X + (G*m_s1./((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s1(1)*ones(len_x,len_x)) + (G*m_s2./((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s2(1)*ones(len_x,len_x)) + (G*m_s3./((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s3(1)*ones(len_x,len_x)));
+    acc_obj_y = -((G*10^(-3)*M./R^3).*Y + (G*m_s1./((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s1(1)*ones(len_x,len_x)) + (G*m_s2./((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s2(1)*ones(len_x,len_x)) + (G*m_s3./((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s3(1)*ones(len_x,len_x)));
+    quiver(X, Y, acc_obj_x, acc_obj_y);
+    hold off;
+    pause(0.0000001)
+    
+    t = t + dt;
+end
+
+% Phase space plot (Draw the acceleration phase space plot for the whole space)
+
