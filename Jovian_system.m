ac69892002f73aea8b49bb22eecb4b4ddad29e02
Xploror <suryansharyan01@gmail.com>
2025-04-20 16:28:55 -0400

    minor fixes

--- Jovian_system.m
+++ Jovian_system.m
@@ -1,20 +1,24 @@
 clc;clear
 
-dt = 0.01;
+dt = 10/1000;
 transfer = 0;
 
-G = 2*10^(-6);
+%G = 2*10^(-6);
+t_scaling = 24*3600;  %1 day in secs
+G = 6.6743*(t_scaling^2)/(421700)^3;
 M = 19000000; % Jupiter  1.9*10^27
 m_s1 = 893;  % Io  8.93*10^22
 m_s2 = 480;  % Europa   4.8*10^22
 m_s3 = 1480;  % Ganymede  14.8*10^22
-m_sat = 0.0001;
+m_sat = 200*1e-20;  %200kg
+
+scaling_jupiter_plot = 3e-4;
 
 resonance_ratio = [2,1];
 e_s1 = 0.0041;  % Io
 e_s2 = 0.009;   % Europa
 e_s3 = 0.0013;  % Ganymede
-e_sat = e_s2;
+e_sat = 0.1;
 a_s1 = 1;  % Io 421700
 a_s2 = 670900/421700;   % Europa  670900
 a_s3 = 1070400/421700;   % Ganymede 1070400
@@ -22,8 +26,17 @@ T_s1 = ((4*pi^2/(G*M))*a_s1^3)^(1/2);
 T_s2 = ((4*pi^2/(G*M))*a_s2^3)^(1/2);
 T_s3 = ((4*pi^2/(G*M))*a_s3^3)^(1/2);
 
-T_sat = resonance_ratio(2)*T_s2/resonance_ratio(1);
-a_sat_target = a_s2*(T_sat/T_s2)^(2/3);
+% Transfer target
+T_sat = resonance_ratio(2)*T_s3/resonance_ratio(1);
+a_sat_target = a_s3*(T_sat/T_s3)^(2/3);
+
+%%%%% Moon orbit pre-plotting
+figure(1);
+ang=0:0.01:2*pi;
+io_orbit = a_s1*[cos(ang); sin(ang)];
+europa_orbit = a_s2*[cos(ang); sin(ang)];
+ganymede_orbit = a_s3*[cos(ang); sin(ang)];
+
 
 r_s1_init = [a_s1*(1+e_s1),0];
 r_s2_init = [a_s2*(1+e_s2),0];
@@ -34,11 +47,11 @@ v3_init = [0,sqrt(G*M*((2/r_s3_init(1))-(1/a_s3)))];
 
 %%%%% Transfer mechanism (Starts with an arbitrary parking orbit around primary 1)
 e1_sat = 0; % Circular orbit (Hyperparameter)
-a1_sat = a_sat_target*(1-e_sat); % (Hyperparameter)
+a1_sat = a_s1*(1-e_sat); % (Hyperparameter)
 T1_sat = ((4*pi^2/(G*M))*a1_sat^3)^(1/2);
 r_sat_init = [a1_sat*(1+e1_sat),0];
 v_sat_init = sqrt(G*M*((2/norm(r_sat_init))-(1/a1_sat)))*cross([0,0,1], [r_sat_init,0])*[1 0 0; 0 1 0]'/norm(r_sat_init);
-t_trans = 40*T1_sat/2;  % (Hyperparameter)
+t_trans = 1*T1_sat;  % (Hyperparameter)
 
 %Dynamic propogation
 r_s1 = r_s1_init;
@@ -51,7 +64,15 @@ r_sat = r_sat_init;
 v_sat = v_sat_init;
 t = 0;
 
-for i=0:dt:100
+% Initial Plotting
+figure(1);
+plot(io_orbit(1,:),io_orbit(2,:),'--r', europa_orbit(1,:),europa_orbit(2,:),'--b', ganymede_orbit(1,:),ganymede_orbit(2,:),'--g');
+title('2D Jovian System')
+xlabel('X (dimensionless)');ylabel('Y (dimensionless)');
+grid on; hold on;
+sat_traj = [];
+
+for i=0:dt:10
     acc_s1 = -((G*M/norm(r_s1)^3)*r_s1 + (G*m_sat/norm(r_sat-r_s1)^3)*(r_s1 - r_sat) + (G*m_s2/norm(r_s2-r_s1)^3)*(r_s1 - r_s2) + (G*m_s3/norm(r_s3-r_s1)^3)*(r_s1 - r_s3));
     acc_s2 = -((G*M/norm(r_s2)^3)*r_s2 + (G*m_sat/norm(r_sat-r_s2)^3)*(r_s2 - r_sat) + (G*m_s1/norm(r_s2-r_s1)^3)*(r_s2 - r_s1) + (G*m_s3/norm(r_s3-r_s2)^3)*(r_s2 - r_s3));
     acc_s3 = -((G*M/norm(r_s3)^3)*r_s3 + (G*m_sat/norm(r_sat-r_s3)^3)*(r_s3 - r_sat) + (G*m_s2/norm(r_s2-r_s3)^3)*(r_s3 - r_s2) + (G*m_s1/norm(r_s3-r_s1)^3)*(r_s3 - r_s1));
@@ -66,28 +87,21 @@ for i=0:dt:100
     r_sat = r_sat + v_sat*dt;
     
     %%%%%%%%%%%%% Transfer mechanism %%%%%%%%%%%%%%%
-%     if t >= t_trans && transfer == 0
-%         a_sat = (norm(r_sat) + a_sat_target*(1+e_sat))/2;
-%         v_sat = sqrt(G*M*((2/norm(r_sat))-(1/a_sat_target)))*cross([0,0,1], [r_sat,0])*[1 0 0; 0 1 0]'/norm(r_sat_init);  % This line of code is analogous to delta_v thrust point
-%         transfer = 1;
-%     end
+    if t >= t_trans && transfer == 0
+        a_sat = (norm(r_sat) + a_sat_target*(1+e_sat))/2;
+        v_sat = sqrt(G*M*((2/norm(r_sat))-(1/a_sat_target)))*cross([0,0,1], [r_sat,0])*[1 0 0; 0 1 0]'/norm(r_sat_init);  % This line of code is analogous to delta_v thrust point
+        transfer = 1;
+        fprintf('Transfer completed!')
+        figure(1)
+        plot(r_sat(1), r_sat(2), '*k');hold on;
+    end
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    figure(1)
-    plot(r_s1(1), r_s1(2), '.r', r_s2(1), r_s2(2), '.b', r_s3(1), r_s3(2), '.g', 0,0, '.k', r_sat(1), r_sat(2), '.y', 'MarkerSize', 20);
+    sat_traj = [sat_traj; r_sat];
+    figure(1);
+    ppp = plot(r_s1(1), r_s1(2), '.r', r_s2(1), r_s2(2), '.b', r_s3(1), r_s3(2), '.g', 0,0, '.k', r_sat(1), r_sat(2), '.y', 'MarkerSize', 20);hold on;
+    plot(sat_traj(:,1), sat_traj(:,2), '--y');
     xlim([-3,3])
     ylim([-3,3])
-    
-    %%%%%% This is the plot to visualize the celestial crossing of the
-    %%%%%% satellite 
-%     if abs((r_s/norm(r_s))*(r_sat/norm(r_sat))' - 1) <= 0.0005
-%         figure(2)
-%         plot(r_s(1), r_s(2), '.b', r_sat(1), r_sat(2), '.g')
-%         xlim([-5,5])
-%         ylim([-5,5])
-%         hold on;
-%     end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % Contour plot
     x = -3:0.05:3;
@@ -95,20 +109,22 @@ for i=0:dt:100
     [X, Y] = meshgrid(x,y);
     len_x = length(X);
     len_y = length(Y);
-    U = -(G*M*10^(-4)./sqrt(X.^2 + Y.^2) + G*m_s1./sqrt((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2) + G*m_s2./sqrt((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2) + G*m_s3./sqrt((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2));
+    U = -(scaling_jupiter_plot*G*M./sqrt(X.^2 + Y.^2) + G*m_s1./sqrt((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2) + G*m_s2./sqrt((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2) + G*m_s3./sqrt((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2));
     figure(2)
     contourf(X, Y, U);
     hold on;
- 
+    
     R = sqrt(X.^2+Y.^2);
-    acc_obj_x = -((G*10^(-3)*M./R^3).*X + (G*m_s1./((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s1(1)*ones(len_x,len_x)) + (G*m_s2./((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s2(1)*ones(len_x,len_x)) + (G*m_s3./((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s3(1)*ones(len_x,len_x)));
-    acc_obj_y = -((G*10^(-3)*M./R^3).*Y + (G*m_s1./((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s1(1)*ones(len_x,len_x)) + (G*m_s2./((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s2(1)*ones(len_x,len_x)) + (G*m_s3./((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s3(1)*ones(len_x,len_x)));
-    quiver(X, Y, acc_obj_x, acc_obj_y);
+    acc_obj_x = -((scaling_jupiter_plot*G*M./R^3).*X + (G*m_s1./((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s1(1)*ones(len_x,len_x)) + (G*m_s2./((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s2(1)*ones(len_x,len_x)) + (G*m_s3./((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2)^(3/2)).*(X - r_s3(1)*ones(len_x,len_x)));
+    acc_obj_y = -((scaling_jupiter_plot*G*M./R^3).*Y + (G*m_s1./((X-r_s1(1)*ones(len_x,len_x)).^2 + (Y-r_s1(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s1(2)*ones(len_y,len_y)) + (G*m_s2./((X-r_s2(1)*ones(len_x,len_x)).^2 + (Y-r_s2(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s2(2)*ones(len_y,len_y)) + (G*m_s3./((X-r_s3(1)*ones(len_x,len_x)).^2 + (Y-r_s3(2)*ones(len_y,len_y)).^2)^(3/2)).*(Y - r_s3(2)*ones(len_y,len_y)));
+    quiver(X, Y, real(acc_obj_x), real(acc_obj_y));
+    title('Contour Plot (Potential Field)');hold on;
+    xlabel('X (dimensionless)');ylabel('Y (dimensionless)');
     hold off;
     pause(0.0000001)
     
     t = t + dt;
+    
+    delete(ppp)
 end
-
-% Phase space plot (Draw the acceleration phase space plot for the whole space)
 
--- cislunar_sim.m
+++ cislunar_sim.m
@@ -3,23 +3,23 @@ clc;clear;
 
 %% Variables
 
-epoch = 5000;
-mu = 1.215*10^(-2);  % mass ratio
+epoch = 1300;
+mu = 1.215*10^(-2);  % Earth-Moon mass ratio 
 major1 = [-mu 0 0];
 major2 = [(1-mu) 0 0];
 t = 0;
-dt = 2*pi*10^(-6)*5*60/2.361;  % taking 5 min time interval for Earth-Moon system
-da = 2*pi*10^(-6)*5*60/2.361; 
+dt = 2*pi*10^(-6)*60*60/2.361;  % taking 5 min time interval for Earth-Moon system
+da = 2*pi*10^(-6)*60*60/2.361; 
 %dt = 0.001;  % Time interval 0.1 was working fine
 %da = 0.001; % angle change
 NB = [cos(t) -sin(t) 0; sin(t) cos(t) 0; 0 0 1];
 
 %% Initial Conditions (Rotating frame)
-pos = [0 -0.1 0];
+pos = [-0.2 -0.1 0];
 x = pos(1);
 y = pos(2);
 z = pos(3);
-vel = 4.24*[cos(-10*pi/180) sin(-10*pi/180) 0]/1.025;  % 26000 kmph ---> 7.22        2.62 giving elliptical orbit for satellite
+vel = 2.62*[cos(-90*pi/180) sin(-90*pi/180) 0]/1.025;  % 26000 kmph ---> 7.22        2.62 giving elliptical orbit for satellite    4.24 is a flyby (-10 deg and [0 -0.1 0])
 velx = vel(1);
 vely = vel(2);
 velz = vel(3);
@@ -28,14 +28,33 @@ X = [major1(1) major2(1)];
 Y = [major1(2) major2(2)];
 Z = [major1(3) major2(3)];
 
-% figure(1);
-% %plot3(X, Y, Z, '.r','MarkerSize', 15);
-% plot(X, Y, '.r','MarkerSize', 10);
-% xlabel('X')
-% ylabel('Y')
-% %zlabel('z')
-% title('Rotating frame')
-% hold on;
+vidobj_iner = VideoWriter("out_iner.jpg", "MPEG-4");
+vidobj_rot = VideoWriter("out_rot.jpg", "MPEG-4");
+vidobj_iner.FrameRate = 30;
+vidobj_rot.FrameRate = 30;
+open(vidobj_iner);
+open(vidobj_rot);
+
+figure(1);
+ang=0:0.01:2*pi; 
+xp=1*cos(ang);
+yp=1*sin(ang);
+
+figure(1);
+plot(X, Y, '.r','MarkerSize', 15);hold on;
+plot((-mu)+xp,yp,'--r');
+xlabel('X')
+ylabel('Y')
+title('Rotating frame')
+grid on;hold on;
+f1 = getframe(gcf);
+writeVideo(vidobj_rot, f1);
+
+figure(2);
+plot3((-mu)+xp,yp,zeros(length(xp),1),'--r');
+grid on;hold on;
+f2 = getframe(gcf);
+writeVideo(vidobj_iner, f2);
 
 %% Numerical integration
 pos_i = NB*pos';
@@ -87,9 +106,12 @@ for i=1:epoch
     plot3(x_i, y_i, z_i, '.g');
     hold on;
     title('Inertial Frame')
-    xlim([-1 1]);
-    ylim([-1 1]);
-    pause(0.0008);
+    xlim([-1.5 1.5]);
+    ylim([-1.5 1.5]);
+    grid on;
+    f2 = getframe(gcf);
+    writeVideo(vidobj_iner, f2);
+    pause(0.0000001);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%% Rotation frame 2D Plotting %%%%%%%%%%%%%%%%%%%
@@ -101,16 +123,16 @@ for i=1:epoch
     plot(x, y, '.b');
     hold on;
     title('Rotating Frame')
-    xlim([-5 5]);
-    ylim([-20 20]);
+    xlim([-1.5 1.5]);
+    ylim([-1.5 1.5]);
     %zlim([-1 1]);
-    pause(0.0008);
+    pause(0.0000001);
+    f1 = getframe(gcf);
+    writeVideo(vidobj_rot, f1);
+    pause(0.0000001);
     
     t = t + da;
     
 end
-
-% state0 = [x, y, z, velx, vely, velz];
-% [time, state] = ode45(@statediffeq, [0,1000*dt], state0);
-% plot(state(:,1), state(:,2))
-% Wierd results for epoch = 2000, pos = [-0.25 0 0], mu = 1.215*10^(-3), dt = 2.661*10^(-6)*3600, vel = 2.552*[cos(-90*pi/180) sin(-90*pi/180) 0]/1.025
+vidobj_iner.close();
+vidobj_rot.close();
